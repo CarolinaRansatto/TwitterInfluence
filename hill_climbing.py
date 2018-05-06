dcfa2538c188cffb91898aca0cc2868c1a60147a
Carlos Henrique <carlos_hdc@hotmail.com>
2018-05-06 15:02:01 -0300

    Hill Climbing alterada 1.0

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,17 +1,29 @@
 import random
 
+def find_k(network,k,heuristic):
+    found = list()
+    for _ in range(k):
+        pos = [random.choice(list(network.keys()))]
+        res = hill_climbing(network,pos[0],found,heuristic,0)
+        if (res!=-1):
+            found.append(res)
+        else:
+            print("deu erro, jesus")
+    return found
 
-def hill_climbing(network, heuristic):
-    pos = [random.choice(list(network.keys()))]
-    global nid
-
-    while pos:
-        print(pos)
-        nid, node = pos[0], network[pos[0]]
-        inf = heuristic(node)
-        pos = node[0]
-        for i in pos:
-            if heuristic(network[i]) <= inf:
-                pos.remove(i)
-
-    return nid
+def hill_climbing(network,pos,found,heuristic,depth):
+    if (depth == 0):
+        n_id,node = pos,network[pos]
+        cur = heuristic(node)
+        id_max = -1
+        for x in range(len(node[0])):
+            n_node = network[node[0][x]]
+            h_node = heuristic(n_node)
+            if ((h_node >= cur) and (node[0][x] not in found)):
+                n_max = h_node
+                id_max = x
+        if (id_max!=-1):
+            return hill_climbing(network,node[0][id_max],found,heuristic,depth)
+        return pos
+    else:
+        pass
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from network import load_network, create_graph
 from heuristics import heuristic1, heuristic2
-from hill_climbing import hill_climbing
+from hill_climbing import hill_climbing,find_k
 
 try:
     network = load_network()
 except FileNotFoundError:
     create_graph()
     network = load_network()
-
-nid = hill_climbing(network, heuristic2)
-
-print('{}: {}'.format(nid, network[nid]))
+maior = -1
+for x in network:
+	if (heuristic1(network[x])>maior):
+		maior = heuristic1(network[x])
+print(maior)
+l = find_k(network,10,heuristic1)
+for x in range(len(l)):
+	print("{} com valor: {}".format(l[x],heuristic1(network[l[x]] ) ) )
