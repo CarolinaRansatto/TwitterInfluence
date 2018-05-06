fa2a42fedbfcf255344403de4774bfd838c36370
Carolina <carolinaransatto@id.uff.br>
2018-05-06 12:49:50 -0300

    Heuristicas iniciadas

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,21 +1,17 @@
 import random
 
 
-def influence(node):
-    return len(node[1])
-
-
-def hill_climbing(network):
+def hill_climbing(network, heuristic):
     pos = [random.choice(list(network.keys()))]
     global nid
 
     while pos:
         print(pos)
         nid, node = pos[0], network[pos[0]]
-        inf = influence(node)
+        inf = heuristic(node)
         pos = node[0]
         for i in pos:
-            if influence(network[i]) <= inf:
+            if heuristic(network[i]) <= inf:
                 pos.remove(i)
 
     return nid
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from network import load_network
+from heuristics import heuristic1, heuristic2
 from hill_climbing import hill_climbing
 
 network = load_network()
-# nid = hill_climbing(network)
+nid = hill_climbing(network, heuristic1)
 
 print(len(network))
 
-# print('{}: {}'.format(nid, network[nid]))
+print('{}: {}'.format(nid, network[nid]))
